f7e00a43283ac2c1b09777da8922d70857623d77
AndriiKhrushch <140267164+AndriiKhrushch@users.noreply.github.com>
2024-10-27 18:48:03 +0200

    JS email and footer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,94 @@
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
-};
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove("show-menu")
-    })
-};
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 };
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const blurHeader = () => {
-    const header = document.getElementById('header');
-    this.scrollY >= 50  ? header.classList.add('blur-header')
-                        : header.classList.remove('blur-header');
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
 };
 
-window.addEventListener('scroll', blurHeader)
+window.addEventListener("scroll", blurHeader);
+
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  emailjs
+    .sendForm(
+      "service_57jckvf",
+      "template_cvk8kyp",
+      "#contact-form",
+      "fIToDM8jIok2s0mQq"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        setTimeout(() => {
+          contactForm.reset();
+        }, 1500);
+      },
+      () => {
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*='" + sectionId + "']"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
